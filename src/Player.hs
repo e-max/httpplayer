7572c8912886f3f313bacdd0132ebe8a0c5d46d5
Max Lavrenov <max.lavrenov@gmail.com>
2013-10-03 17:15:56 +0400

    разнес функи

--- src/Player.hs
+++ src/Player.hs
@@ -4,9 +4,14 @@ module Main (main) where
 --import Control.Monad.Base (liftBase)
 import Network.HTTP.Conduit --(ManagerSettings, Manager, newManager, def, Request(..), parseUrl, RequestBody(RequestBodyBS),httpLbs, responseBody)
 --import Control.Monad.IO.Class (MonadIO (liftIO))
+import Network.HTTP.Types (RequestHeaders)
 import qualified Data.ByteString.Lazy as L
 import Data.Conduit
 import qualified Data.CaseInsensitive as CI
+import Control.Concurrent
+import Control.Monad
+import Data.ByteString
+
 
 --import Network.HTTP.Types (StdMethod(POST))
 --
@@ -15,13 +20,22 @@ body = "{\"advwatchid\":\"689340710147.0479_829816917091.873\",\"_domain\":\"www
 
 main :: IO ()
 main = do
-        defaultReq <- parseUrl "http://localhost:4444/logger/adv/got/"
         let managerSettings = (def)::ManagerSettings
-            headers = [(CI.mk "Content-Type", "application/json")]
-            req = defaultReq {method="POST", rawBody=True, requestBody=(RequestBodyBS body), requestHeaders = headers}
+        let headers = [(CI.mk "Content-Type", "application/json")]
+        let url = "http://localhost:4444/logger/adv/got/"
+
         manager <- newManager managerSettings
-        runResourceT $  fmap responseBody $ httpLbs req manager
-        print "TEST"
+
+        res <- runQuery manager url headers body
+        replicateM_ 10 $ forkIO $ runQuery manager url headers body
+        print "RUN"
+
+runQuery:: Manager -> String -> RequestHeaders -> ByteString -> IO ()
+runQuery manager url headers body = do
+        defaultReq <- parseUrl url
+        let req = defaultReq {method="POST", rawBody=True, requestBody=(RequestBodyBS body), requestHeaders = headers}
+        resp <- runResourceT $  fmap responseBody $ httpLbs req manager
+        L.putStrLn resp
 
 
 {-runQuery :: MonadIO m => Manager -> Request m-> m L.ByteString-}
